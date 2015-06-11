527508e3c3b8260eea24d2471b41ed7666c71833
qihao <sqqihao@gmail.com>
2015-06-11 14:12:47 +0800

    #模拟的select值发生改变时， 实际上的select并没有触发change事件， #；
    手动触发select的事件：
     //需要手懂触发select的change事件;
     $(self.select[0]).trigger("change");

--- select.js
+++ select.js
@@ -220,6 +220,9 @@
 
 			self.select[0].selectedIndex = index;
 
+            //需要手懂触发select的change事件;
+            $(self.select[0]).trigger("change");
+
 			self._value.html(st.html());
 
 			self.close();
